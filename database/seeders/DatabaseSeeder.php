ef5ccb8444ef57ebcf8c4281989fd0a057466330
Dheeraj <dheerajagarwal1995@gmail.com>
2023-10-30 00:14:44 +0530

    added roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Role;
+use App\Models\User;
+use Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'user']);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $adminRole = Role::where('name', 'admin')->first();
+        $userRole = Role::where('name', 'user')->first();
+
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin@123'),
+            'role_id' => $adminRole->id,
+        ]);
+        
+        User::create([
+            'name' => 'Customer 1',
+            'email' => 'customer1@gmail.com',
+            'password' => Hash::make('customer@123'),
+            'role_id' => $userRole->id,
+        ]);
+        
+        User::create([
+            'name' => 'Customer 2',
+            'email' => 'customer2@gmail.com',
+            'password' => Hash::make('customer@123'),
+            'role_id' => $userRole->id,
+        ]);
     }
 }
